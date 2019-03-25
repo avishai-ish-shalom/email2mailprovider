ca8309353ecb548eb8f632d8f32fbb95d8abf699
Avishai Ish-Shalom <avishai.ishshalom@gmail.com>
2019-03-25 13:25:33 +0200

    Remove dead code

--- test_smtp_readfile_multidns.py
+++ test_smtp_readfile_multidns.py
@@ -9,10 +9,8 @@ import time
 
 conn = sqlite3.connect('scanner.db')
 
-my_resolver = dns.resolver.Resolver()
 dns_server_list = ['209.244.0.3', '84.200.69.80', '8.26.56.26', '208.67.222.222', '156.154.70.1', '199.85.126.10', '81.218.119.11',
                    '195.46.39.39', '107.150.40.234', '208.76.50.50', '216.146.35.35', '37.235.1.174', '89.233.43.71', '74.82.42.42', '109.69.8.51']
-dnscnt = 1
 
 infile = open(sys.argv[1], 'r')
 
@@ -29,13 +27,7 @@ for line in infile:
     print("Found %s records for %s" % (a[0], line))
 
     if (0 == a[0]):
-        if (0 == (dnscnt % len(dns_server_list))):
-            dnscnt = 1
-            time.sleep(1)
         try:
-            print(dns_server_list[dnscnt-1])
-            my_resolver_nameservers = [dns_server_list[dnscnt-1]]
-            dnscnt += 1
             mx_dns_resp = dns.resolver.query(str(line), 'MX')
             for rdata in mx_dns_resp:
                 print(line, str(rdata.exchange))
